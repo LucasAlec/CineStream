85f74b2d33105cd801deabde529ba2f761790b55
Lucas Alecsander <lucasalecsander10@hotmail.com>
2024-10-17 14:23:50 -0300

    first commit

---
+++ src/CineStream.java
@@ -0,0 +1,8 @@
+public class CineStream {
+    public static void main(String[] args) {
+        // Iniciar a aplicação
+        //Criar um novo objeto "MenuView"
+
+        //Exibir a view
+    }
+}
