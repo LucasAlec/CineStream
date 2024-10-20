4f9849803f387596b0330d40fe62392f2784fa66
Lucas <lucassalvadorcarmo@gmail.com>
2024-10-20 17:39:30 -0300

    lançamento das excepctions

---
+++ src/exception/OpcaoInvalidaException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class OpcaoInvalidaException extends RuntimeException {
+    public OpcaoInvalidaException(String message) {
+        super(message);
+    }
+}
--- src/util/PaginacaoUtil.java
+++ src/util/PaginacaoUtil.java
@@ -128,7 +128,6 @@ public class PaginacaoUtil {
                     break;
             }
         }
-    }}
-
+    }
 }
 
